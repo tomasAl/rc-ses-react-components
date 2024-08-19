5dc0cdf5236fa75d0f2dff481741b1108a3a826e
Vytautas Luksys <vytautas.luksys@cgi.com>
2024-08-19 17:06:35 +0300

    feat: checkbox component

--- src/components/form/input/Checkbox.tsx
+++ src/components/form/input/Checkbox.tsx
@@ -1,25 +1,54 @@
 import {
-  FormControl,
   FormControlLabel,
   Checkbox as MuiCheckbox,
   CheckboxProps as MuiCheckboxProps,
+  styled,
 } from '@mui/material'
 import { forwardRef } from 'react'
 import { FieldError } from 'react-hook-form'
 
 import CheckBoldIcon from '@/assets/icons/CheckBoldIcon'
 import CheckUncheckedBoldIcon from '@/assets/icons/CheckUncheckedBoldIcon'
+import palette from '@/theme/palette'
+
+import RcSesFormControlWrapper from '../FormControlWrapper'
+
+const StyledFormControlLabel = styled(FormControlLabel)({
+  backgroundColor: palette.grey['50'],
+  borderColor: palette.grey['500'],
+  borderStyle: 'solid',
+  borderWidth: '1px',
+  borderRadius: '.25rem',
+  margin: 0,
+  padding: '1rem 1.25rem',
+
+  '.MuiTypography-root': {
+    fontSize: '.875rem !important',
+    lineHeight: '1.25rem !important',
+  },
+})
 
 type Props = MuiCheckboxProps & {
   children: React.ReactNode
+  description?: string
   errors?: FieldError | undefined
+  label?: string
+  labelSubtitle?: string
 }
 
 const RcSesCheckbox = forwardRef<HTMLInputElement, Props>((props, ref) => {
-  const { children, errors, ...checkboxProps } = props
+  const { children, description, label, labelSubtitle, ...checkboxProps } = props
+  const id = props.id ?? crypto.randomUUID()
+
   return (
-    <FormControl error={!!errors}>
-      <FormControlLabel
+    <RcSesFormControlWrapper
+      description={description}
+      errors={props.errors}
+      id={id}
+      label={label}
+      labelSubtitle={labelSubtitle}
+    >
+      <StyledFormControlLabel
         control={
           <MuiCheckbox
             {...checkboxProps}
@@ -31,12 +60,15 @@ const RcSesCheckbox = forwardRef<HTMLInputElement, Props>((props, ref) => {
         }
         label={children}
       />
-    </FormControl>
+    </RcSesFormControlWrapper>
   )
 })
 
 RcSesCheckbox.defaultProps = {
+  description: undefined,
   errors: undefined,
+  label: undefined,
+  labelSubtitle: undefined,
 }
 
 export default RcSesCheckbox
---
+++ src/components/form/input/CheckboxInput.tsx
@@ -0,0 +1,42 @@
+import {
+  FormControl,
+  FormControlLabel,
+  Checkbox as MuiCheckbox,
+  CheckboxProps as MuiCheckboxProps,
+} from '@mui/material'
+import { forwardRef } from 'react'
+import { FieldError } from 'react-hook-form'
+
+import CheckBoldIcon from '@/assets/icons/CheckBoldIcon'
+import CheckUncheckedBoldIcon from '@/assets/icons/CheckUncheckedBoldIcon'
+
+type Props = MuiCheckboxProps & {
+  children: React.ReactNode
+  errors?: FieldError | undefined
+}
+
+const RcSesCheckboxInput = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  const { children, errors, ...checkboxProps } = props
+  return (
+    <FormControl error={!!errors}>
+      <FormControlLabel
+        control={
+          <MuiCheckbox
+            {...checkboxProps}
+            checkedIcon={<CheckBoldIcon />}
+            disableRipple
+            icon={<CheckUncheckedBoldIcon />}
+            inputRef={ref}
+          />
+        }
+        label={children}
+      />
+    </FormControl>
+  )
+})
+
+RcSesCheckboxInput.defaultProps = {
+  errors: undefined,
+}
+
+export default RcSesCheckboxInput
